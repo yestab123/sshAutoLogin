4e6cc31109c45d75e49e04282ea229f6a1be825a
root <yestab123@gmail.com>
2017-03-16 10:47:22 +0800

    Init

---
+++ ssh.py
@@ -0,0 +1,88 @@
+#!/usr/bin/python
+import os
+
+def ssh_password(name, ip, port, user, password):
+    buff = '''#!/usr/bin/expect -f
+      set user %s
+      set host %s
+      set password %s
+      set port %d
+      set timeout -1
+
+      spawn ssh $user@$host -p $port
+      expect "*assword:*"
+      send "$password\r"
+      interact
+      expect eof''' % (str(user), str(ip), str(password), int(port))
+
+    fd = open("%s.sh" % str(name), "w")
+    fd.write(buff)
+    fd.close()
+
+    os.system("chmod 700 %s.sh" % (str(name)))
+
+def ssh_key(name, ip, port, user, password, key):
+    buff = '''#!/usr/bin/expect -f
+      set user %s
+      set host %s
+      set password %s
+      set port %d
+      set timeout -1
+      set key %s
+
+      spawn ssh -i $key $user@$host -p $port
+      expect "*passphrase*"
+      send "$password\r"
+      interact
+      expect eof''' % (str(user), str(ip), str(password), int(port), str(key))
+
+    fd = open("%s.sh" % str(name), "w")
+    fd.write(buff)
+    fd.close()
+
+    os.system("chmod 700 %s.sh" % (str(name)))
+
+def ssh_password_sh(name, ip, port, user, password):
+    buff = '''#!/bin/bash
+    ./login/password_login %s %d %s %s''' % (str(ip), int(port), str(user), str(password))
+
+    fd = open("%s.sh" % str(name), "w")
+    fd.write(buff)
+    fd.close()
+
+    os.system("shc -v -f %s.sh" % str(name))
+    os.system("rm -f %s.sh.x.c" % str(name))
+    os.system("mv %s.sh.x %s.sh" % (str(name), str(name)))
+    os.system("chmod 700 %s.sh" % (str(name)))
+
+
+def ssh_key_sh(name, ip, port, user, password, key):
+    buff = '''#!/bin/bash
+    ./login/key_login %s %d %s %s %s''' % (str(ip), int(port), str(user), str(password), str(key))
+
+    fd = open("%s.sh" % str(name), "w")
+    fd.write(buff)
+    fd.close()
+    
+    os.system("shc -v -f %s.sh" % str(name))
+    os.system("rm -f %s.sh.x.c" % str(name))
+    os.system("mv %s.sh.x %s.sh" % (str(name), str(name)))
+    os.system("chmod 700 %s.sh" % (str(name)))
+
+if __name__ == '__main__':
+    sh_name = raw_input("ssh name:")
+    sh_ip   = raw_input("ssh ip:")
+    sh_port = raw_input("ssh port(default 22):")
+    if sh_port == '':
+        sh_port = 22
+    sh_user = raw_input("ssh user:")
+    sh_work_s = raw_input("ssh work(0=password, 1=key, default password):")
+    if sh_work_s == '':
+        sh_work_s = 0
+    sh_password = raw_input("ssh(key) password:")
+    sh_work = int(sh_work_s)
+    if sh_work == 0:
+        ssh_password_sh(sh_name, sh_ip, sh_port, sh_user, sh_password)
+    else:
+        sh_key = raw_input("ssh key path:")
+        ssh_key_sh(sh_name, sh_ip, sh_port, sh_user, sh_password, sh_key)
